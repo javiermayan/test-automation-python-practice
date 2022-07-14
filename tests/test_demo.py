1689c41fdc08da2eae2c3dc6749a3527d8668706
Javier Mayan <sitiosmayan@gmail.com>
2022-07-14 17:24:16 -0300

    testing

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -8,4 +8,19 @@ def test_the_tests():
 
   assert 2==3
 
-#/home/runner/work/test-automation-python-practice/test-automation-python-practice
+
+"""
+This script will open the Qxf2 website and verify its title
+"""
+from selenium import webdriver
+
+site_url = 'https://qxf2.com/'
+
+
+def test_site_title():
+  "Checks Qxf2's website title"
+  driver = webdriver.Chrome()
+  driver.get(site_url)
+  assert driver.title == 'Qxf2 Services: Outsourced Software QA for startups'
+
+
