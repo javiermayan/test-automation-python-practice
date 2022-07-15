0dacab7777cc40d9f6b4feb55d37f1564c7b05db
Javier Mayan <sitiosmayan@gmail.com>
2022-07-15 13:37:17 -0300

    testing

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,10 +1,24 @@
 import unittest
+'''
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+'''
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromiumService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+
+'''
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-import time
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-
+from webdriver_manager.utils import ChromeType
+'''
+import time
 
 
 class Ayuda(unittest.TestCase):
@@ -13,16 +27,38 @@ class Ayuda(unittest.TestCase):
         global driver
         #s = Service("../Drivers/chromedriver")
         #driver = webdriver.Chrome(service=s)
-        driver = webdriver.Chrome(ChromeDriverManager().install())
+        #driver = webdriver.Chrome(ChromeDriverManager().install())
+        '''
+        driver_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+        driver = webdriver.Chrome(driver_path)
         driver.maximize_window()
+        '''
+        driver = webdriver.Chrome(
+            service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+        '''
+        chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+                
+        chrome_options = Options()
+        options = [
+            "--headless",
+            "--disable-gpu",
+            "--window-size=1920,1200",
+            "--ignore-certificate-errors",
+            "--disable-extensions",
+            "--no-sandbox",
+            "--disable-dev-shm-usage"
+        ]
+        for option in options:
+            chrome_options.add_argument(option)
+
+        driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+        '''
         driver.get("https://www.google.com")
+        print(driver.title)
 
     def test_site_title(self):
         time.sleep(5)  # Let the user actually see something!
         assert driver.title == 'Google'
-
-
-
 
 
     def test_the_tests(self):
